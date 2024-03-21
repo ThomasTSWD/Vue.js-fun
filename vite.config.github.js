03b650c3c49494a231888a880b47fc6510890367
Tommy TSWD <54892623+ThomasTSWD@users.noreply.github.com>
2024-03-21 12:50:39 +0000

    test 2 vite.config

---
+++ vite.config.github.js
@@ -0,0 +1,21 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  // !!!
+  //base: 'dist/' // local
+  base: '/Vue.js-fun/'// pour github pages
+  //base vide pour Pour netlify 
+  //base: '/'
+})
